c1eadcead73c2a77f34a03347627a07f20207f02
Claudio Costa <claudio@imaginaction.com.br>
2019-10-05 18:13:50 -0300

    feet: 04 - 01 - Criando a tela de resumo da compra

--- app/src/main/java/br/com/alura/aluraviagens/ui/activity/PagamentoActivity.java
+++ app/src/main/java/br/com/alura/aluraviagens/ui/activity/PagamentoActivity.java
@@ -1,5 +1,6 @@
 package br.com.alura.aluraviagens.ui.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -28,6 +29,9 @@ public class PagamentoActivity extends AppCompatActivity {
                 new BigDecimal("243.99"));
 
         mostraPreco(pacoteSaoPaulo);
+
+        Intent intent = new Intent(this, ResumoCompraActivity.class);
+        startActivity(intent);
 
     }
 
