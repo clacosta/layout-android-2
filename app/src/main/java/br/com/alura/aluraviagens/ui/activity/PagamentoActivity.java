7c7ff06a32ce545e07dd992cc5bb674b2f3ff975
Claudio Costa <claudio@imaginaction.com.br>
2019-10-05 17:11:28 -0300

    feet: 03 - 07 - Limitando dígitos e realizando o bind

--- app/src/main/java/br/com/alura/aluraviagens/ui/activity/PagamentoActivity.java
+++ app/src/main/java/br/com/alura/aluraviagens/ui/activity/PagamentoActivity.java
@@ -2,14 +2,38 @@ package br.com.alura.aluraviagens.ui.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.math.BigDecimal;
 
 import br.com.alura.aluraviagens.R;
+import br.com.alura.aluraviagens.model.Pacote;
+import br.com.alura.aluraviagens.util.MoedaUtil;
 
 public class PagamentoActivity extends AppCompatActivity {
+
+    public static final String TITULO_APPBAR = "Pagemanto";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pagamento);
+
+        setTitle(TITULO_APPBAR);
+
+        Pacote pacoteSaoPaulo = new Pacote(
+                "São Paulo",
+                "sao_paulo_sp",
+                2,
+                new BigDecimal("243.99"));
+
+        mostraPreco(pacoteSaoPaulo);
+
+    }
+
+    private void mostraPreco(Pacote pacote) {
+        TextView preco = findViewById(R.id.resumo_pacote_preco);
+        String moedaBrasileira = MoedaUtil.formataParaBrasileira(pacote.getPreco());
+        preco.setText(moedaBrasileira);
     }
 }
